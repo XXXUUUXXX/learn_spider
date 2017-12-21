ade80eb6b9cbc3df76dfc6fd1682c11183be49d6
tian <963766613@qq.com>
2017-12-22 00:20:58 +0800

    12-22

---
+++ tencentspider/tencentspider/items.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# http://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class TencentspiderItem(scrapy.Item):
+    # 职位名
+    position_name = scrapy.Field()
+    # 详情链接
+    position_link = scrapy.Field()
+    # 职位类别
+    position_type = scrapy.Field()
+    # 招聘人数
+    position_num = scrapy.Field()
+    # 工作地点
+    work_location = scrapy.Field()
+    # 发布时间
+    publish_time = scrapy.Field()
